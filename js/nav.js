433e74aa56565d2fa74011b06b89ed9c615bde0a
dkmullen <dkmullen@gmail.com>
2016-01-21 06:32:32 -0500

    Add functionality to show active links, dynamically render page title.

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,15 @@
 var menuList = [
-	{name: "Intro", link: "intro.html"},
-	{name: "Lesson 1", link: "les1.html"},
-	{name: "Lesson 2", link: "les2.html"},
-	{name: "Lesson 3", link: "les3.html"},
-	{name: "Lesson 4", link: "les4.html"}
+	{name: "Intro", link: "intro.html", cssClass: "b"},
+	{name: "Lesson 1", link: "les1.html", cssClass: "b"},
+	{name: "Lesson 2", link: "les2.html", cssClass: "b"},
+	{name: "Lesson 3", link: "les3.html", cssClass: "b"},
+	{name: "Lesson 4", link: "les4.html", cssClass: "b"}
 ];
 
 var MenuItem = function(data) {
 	this.name = ko.observable(data.name);
 	this.link = ko.observable(data.link);
+	this.cssClass = ko.observable(data.cssClass);
 };
 
 var ViewModel = function() {
@@ -17,19 +18,31 @@ var ViewModel = function() {
 	//Creates an empty array to hold menu objects
 	this.menu = ko.observableArray([]);
 	
+	var url = window.location.pathname;
+	var filename = url.substring(url.lastIndexOf('/')+1);
+	
 	//Adds each menu item to the array 
 	menuList.forEach(function(pageTab){
+		if (pageTab.link == filename) {
+			pageTab.cssClass = "active";
+			document.getElementById('title').innerHTML = pageTab.name;
+		}
 		self.menu.push( new MenuItem(pageTab) );
 	});
 	
 	document.getElementById('nav-links').innerHTML =
 		'<ul data-bind="foreach: menu">' +
 		'<a data-bind="attr: {href: link}">' +
-		'<li data-bind="text: name"></li></a></ul>';
+		'<li data-bind="text: name, attr: {class: cssClass}"></li></a></ul>';
 	
 	document.getElementById('footer-links').innerHTML = 
 		'<ul data-bind="foreach: menu">' +
 		'<a data-bind="attr: {href: link}">' +
-		'<li data-bind="text: name"></li></a></ul>';
+		'<li data-bind="text: name, attr: {class: cssClass}"></li></a></ul>';
+		
+	
+	var x = document.getElementsByTagName('a');
 };
+
+
 ko.applyBindings(new ViewModel())
